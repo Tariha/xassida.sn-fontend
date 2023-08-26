8fcf5ebab90728a6bbe724b9999cfb0d1e6cebd4
Maboury Niang <nmaboury@gmail.com>
2023-08-26 21:00:56 +0100

    Fix Payment amoutn input isssue

--- app/contribute/components/core/TitleTile.tsx
+++ app/contribute/components/core/TitleTile.tsx
@@ -13,7 +13,9 @@ const TitleTile = ({ title, subtitle }: Props) => {
   return (
     <>
       <div className="mb-6 text-4xl">{title}</div>
-      <ReactMarkdown className="mb-8" linkTarget="_blank">{subtitle}</ReactMarkdown>
+      <ReactMarkdown className="mb-8" linkTarget="_blank">
+        {subtitle}
+      </ReactMarkdown>
     </>
   )
 }
