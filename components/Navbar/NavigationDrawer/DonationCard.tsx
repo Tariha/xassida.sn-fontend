921154958bed870e6b6467f28af4daaeee87ec7d
Linzo99 <sallalioune@gmail.com>
2023-12-04 11:24:35 +0000

    donation through Kopar

--- components/Navbar/NavigationDrawer/DonationCard.tsx
+++ components/Navbar/NavigationDrawer/DonationCard.tsx
@@ -10,17 +10,24 @@ import {
 const DonationCard = () => (
   <Card>
     <CardHeader>
-      <CardTitle className="text-xl">Become a monthly donator</CardTitle>
+      <CardTitle className="text-xl">Soutenez le Projet</CardTitle>
     </CardHeader>
     <CardContent>
       <p>
-        Monthly donations help us improve Xassida.sn and sustain operations so
-        we focus less on fundraising and more on creating impact.
+        {
+          "Votre don contribue à préserver, diffuser et améliorer l'expérience Xassida pour des milliers de personnes à travers le monde. Chaque contribution, grande ou petite, façonne notre communauté spirituelle."
+        }
       </p>
     </CardContent>
     <CardFooter>
       <Button size="lg" className="text-md bg-vert hover:bg-vert/90">
-        Donate Monthly
+        <a
+          href="https://koparexpress.com/apps/collectes/r830jt9qe"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Contribuer
+        </a>
       </Button>
     </CardFooter>
   </Card>
